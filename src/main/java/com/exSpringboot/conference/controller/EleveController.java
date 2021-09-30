745aaed9d998a5ef26d5eaaa1d6455d0a3546305
NoxxDonovan <donovanbrun@gmail.com>
2021-09-30 10:05:31 +0200

    update

--- src/main/java/com/exSpringboot/conference/controller/EleveController.java
+++ src/main/java/com/exSpringboot/conference/controller/EleveController.java
@@ -1,6 +1,6 @@
 package com.exSpringboot.conference.controller;
 
-import com.exSpringboot.conference.manager.EleveManager;
+import com.exSpringboot.conference.service.EleveService;
 import com.exSpringboot.conference.model.Eleve;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,30 +11,32 @@ import java.util.List;
 @RequestMapping(path = "eleve")
 public class EleveController {
 
-    private final EleveManager eleveManager;
+    private final EleveService eleveService;
 
     @Autowired
-    public EleveController(EleveManager eleveManager) {
-        this.eleveManager = eleveManager;
+    public EleveController(EleveService eleveService) {
+        this.eleveService = eleveService;
     }
 
     @GetMapping
     public List<Eleve> getAll() {
-        return eleveManager.getEleves();
+        return eleveService.getEleves();
     }
 
     @GetMapping(path = "{index}")
     public Eleve getOne(@PathVariable int index) {
-        return eleveManager.getEleve(index);
+        return eleveService.getEleve(index);
     }
 
+    /*
     @PostMapping
     public void add(@RequestBody Eleve eleve) {
-        if (eleve != null) eleveManager.addEleve(eleve);
+        if (eleve != null) eleveService.addEleve(eleve);
     }
 
     @DeleteMapping(path = "{index}")
     public void delete(@PathVariable int index) {
-        eleveManager.deleteEleve(index);
+        eleveService.deleteEleve(index);
     }
+    */
 }
